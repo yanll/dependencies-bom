a783da700d5f83ba09294973ee5006e0913ecb48
yanll <m3GAGGM7>
2017-02-16 15:21:43 +0800

    M

--- yanll-pull.bat
+++ yanll-pull.bat
@@ -5,6 +5,8 @@ cd D:\workspace\frameworks
 git pull
 cd D:\workspace\platforms
 git pull
+cd D:\workspace\platforms-ui
+git pull
 cd D:\workspace\javadev-ideas
 git pull
 cd D:\workspace\rabbitmqdemo
