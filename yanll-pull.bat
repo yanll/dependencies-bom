0ffbfd202c92c8f99c135bce4c86a710e4a05e36
yanll <m3GAGGM7>
2017-03-23 18:54:02 +0800

    M

--- yanll-pull.bat
+++ yanll-pull.bat
@@ -13,4 +13,10 @@ cd D:\workspace\rabbitmqdemo
 git pull
 cd D:\workspace\springbootdemo
 git pull
+cd D:\workspace\hadoopdemo
+git pull
+cd D:\workspace\nettydemo
+git pull
+cd D:\workspace\springcloud-demo
+git pull
 pause
